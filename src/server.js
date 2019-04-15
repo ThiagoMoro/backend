7a8b49895c1093e14dcd88402e4f768ca368630f
Thiago Moro <thiago.g.moro@gmail.com>
2019-04-15 20:31:57 -0300

    Adiciona variaveis ambiente

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ io.on("connection", socket => {
 });
 
 mongoose.connect(
-    'mongodb+srv://omnistack:omnistack@cluster0-m0rbo.mongodb.net/test?retryWrites=true', 
+    'mongodb+srv://thiago:thiago@cluster0-m0rbo.mongodb.net/test?retryWrites=true', 
     {
         useNewUrlParser: true
     }
